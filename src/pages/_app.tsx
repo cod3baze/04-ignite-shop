623878e7860db2e268e71b9b684f5e872e9897da
elias alexandre <elliasallex@gmail.com>
2022-11-11 10:34:02 -0300

    Next Images

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,20 @@
 import type { AppProps } from "next/app";
+import Image from "next/image";
 
 import { globalStyles } from "../styles/global";
+import logoImage from "../assets/logo.svg";
+import { Container, Header } from "../styles/pages/app";
+
+globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  globalStyles();
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImage} alt="Ignite Shop Logo" />
+      </Header>
 
-  return <Component {...pageProps} />;
+      <Component {...pageProps} />
+    </Container>
+  );
 }
